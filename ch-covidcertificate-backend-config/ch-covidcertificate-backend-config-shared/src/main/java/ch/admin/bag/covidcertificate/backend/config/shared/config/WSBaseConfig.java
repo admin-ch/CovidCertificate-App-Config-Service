8367a699f3fed79d1fb0e2b649b8c22d1939bb66
Patrick Amrein <amrein@ubique.ch>
2021-05-26 10:58:53 +0200

    Add first impl of JWSMessageConverter

--- ch-covidcertificate-backend-config/ch-covidcertificate-backend-config-shared/src/main/java/ch/admin/bag/covidcertificate/backend/config/shared/config/WSBaseConfig.java
+++ ch-covidcertificate-backend-config/ch-covidcertificate-backend-config-shared/src/main/java/ch/admin/bag/covidcertificate/backend/config/shared/config/WSBaseConfig.java
@@ -14,8 +14,21 @@ import ch.admin.bag.covidcertificate.backend.config.shared.helper.CacheUtil;
 import ch.admin.bag.covidcertificate.backend.config.shared.helper.FaqHelper;
 import ch.admin.bag.covidcertificate.backend.config.shared.interceptor.HeaderInjector;
 import ch.admin.bag.covidcertificate.backend.config.shared.poeditor.Messages;
+import ch.admin.bag.covidcertificate.backend.config.shared.security.signature.JWSMessageConverter;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 import java.time.Duration;
+import java.util.Base64;
+import java.util.List;
 import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -23,6 +36,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -39,6 +53,11 @@ public abstract class WSBaseConfig implements WebMvcConfigurer {
     public void setConfigMaxAge(Duration configMaxAge) {
         CacheUtil.CONFIG_MAX_AGE = configMaxAge;
     }
+
+    @Value("${ws.jws.p12:}")
+    public String p12KeyStore;
+    @Value("${ws.jws.password:}")
+    public String p12KeyStorePassword;
 
     @Bean
     public HeaderInjector securityHeaderInjector() {
@@ -70,5 +89,27 @@ public abstract class WSBaseConfig implements WebMvcConfigurer {
         messageSource.setFallbackToSystemLocale(false);
         messageSource.setDefaultLocale(null);
         return messageSource;
+    }
+
+    @Bean
+    public JWSMessageConverter jwsMessageConverter() throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException, UnrecoverableKeyException {
+        return new JWSMessageConverter(jwsKeyStore(), p12KeyStorePassword.toCharArray());
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        try {
+            converters.add(jwsMessageConverter());
+        } catch (KeyStoreException | NoSuchAlgorithmException | CertificateException | IOException | UnrecoverableKeyException e) {
+            logger.error("Could not load key store", e);
+            throw new RuntimeException("Could not add jws Converter");
+        }
+    }
+
+    public KeyStore jwsKeyStore() throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
+        var keyStore = KeyStore.getInstance("pkcs12");
+        var bais = new ByteArrayInputStream(Base64.getDecoder().decode(p12KeyStore));
+        keyStore.load(bais, p12KeyStorePassword.toCharArray());
+        return keyStore;
     }
 }
