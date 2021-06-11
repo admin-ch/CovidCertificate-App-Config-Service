6bb4d7c35f95b32c669bcd058343834865493fce
Felix Haller <haller@ubique.ch>
2021-06-11 14:43:13 +0200

    add faq mock link

---
+++ ch-covidcertificate-backend-config/ch-covidcertificate-backend-config-verifier-ws/src/main/java/ch/admin/bag/covidcertificate/backend/config/verifier/ws/config/mock/MockFaqLinkConfig.java
@@ -0,0 +1,53 @@
+package ch.admin.bag.covidcertificate.backend.config.verifier.ws.config.mock;
+
+import ch.admin.bag.covidcertificate.backend.config.shared.helper.FaqHelper;
+import ch.admin.bag.covidcertificate.backend.config.shared.model.ConfigResponse;
+import ch.admin.bag.covidcertificate.backend.config.shared.model.Faq;
+import ch.admin.bag.covidcertificate.backend.config.shared.model.FaqEntry;
+import ch.admin.bag.covidcertificate.backend.config.shared.model.Language;
+import ch.admin.bag.covidcertificate.backend.config.shared.poeditor.Messages;
+import ch.admin.bag.covidcertificate.backend.config.verifier.ws.controller.VerifierConfigController;
+import java.util.Map;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
+
+@Configuration
+@Profile("mock-faqlink")
+public class MockFaqLinkConfig {
+
+    @Bean
+    @Primary
+    public VerifierConfigController verifierConfigController(
+            Messages messages, FaqHelper faqHelper) {
+        return new MockConfigController(messages, faqHelper);
+    }
+
+    public class MockConfigController extends VerifierConfigController {
+
+        public MockConfigController(Messages messages, FaqHelper faqHelper) {
+            super(messages, faqHelper);
+        }
+
+        @Override
+        public String hello() {
+            return super.hello() + " (mock-faqlink)";
+        }
+
+        @Override
+        public ResponseEntity<ConfigResponse> getConfig(
+                String appversion, String osversion, String buildnr) {
+            ResponseEntity<ConfigResponse> response =
+                    super.getConfig(appversion, osversion, buildnr);
+            Map<Language, Faq> works = response.getBody().getWorks();
+            for (Faq faq : works.values()) {
+                FaqEntry faqEntry = faq.getFaqEntries().get(0);
+                faqEntry.setLinkUrl("https://www.ubique.ch");
+                faqEntry.setLinkTitle("mock link");
+            }
+            return response;
+        }
+    }
+}
