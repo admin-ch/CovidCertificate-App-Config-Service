9a45e079c20e1b53482cbd52b44d983f0f5e4382
Martin Alig <alig@ubique.ch>
2021-06-22 14:10:39 +0200

    New FAQs

--- ch-covidcertificate-backend-config/ch-covidcertificate-backend-config-shared/src/main/java/ch/admin/bag/covidcertificate/backend/config/shared/helper/FaqHelper.java
+++ ch-covidcertificate-backend-config/ch-covidcertificate-backend-config-shared/src/main/java/ch/admin/bag/covidcertificate/backend/config/shared/helper/FaqHelper.java
@@ -18,11 +18,11 @@ public class FaqHelper {
     private static final String WALLET_KEY_PREFIX_WORKS = "wallet_faq_works_";
 
     private static final List<String> WALLET_FAQ_QUESTIONS_ENTRIES =
-            List.of("1", "1_1", "2", "3", "4", "5");
+            List.of("1", "1_1", "2", "2_1", "3", "4", "5");
     private static final List<String> WALLET_FAQ_WORKS_ENTRIES =
-            List.of("1", "2", "3", "4", "5", "6");
+            List.of("1", "2", "3", "3_1", "4", "5", "6");
 
-    private static final List<String> VERIFIER_WORKS_FAQ_ENTRIES = List.of("1", "2", "3", "5", "6");
+    private static final List<String> VERIFIER_WORKS_FAQ_ENTRIES = List.of("1", "2", "2_1", "3", "5", "6");
 
     private final Messages msg;
 
