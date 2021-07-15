f742855441620ed896142b7e04e3c2ddf042cf84
Felix Haller <haller@ubique.ch>
2021-07-15 15:11:04 +0200

    add another how it works faq for verifier

--- ch-covidcertificate-backend-config/ch-covidcertificate-backend-config-shared/src/main/java/ch/admin/bag/covidcertificate/backend/config/shared/helper/FaqHelper.java
+++ ch-covidcertificate-backend-config/ch-covidcertificate-backend-config-shared/src/main/java/ch/admin/bag/covidcertificate/backend/config/shared/helper/FaqHelper.java
@@ -26,7 +26,7 @@ public class FaqHelper {
     private static final List<String> WALLET_FAQ_WORKS_ENTRIES =
             List.of("1", "2", "3", "3_1", "4", "5", "6");
     private static final List<String> VERIFIER_WORKS_FAQ_ENTRIES =
-        List.of("1", "2", "2_1", "3", "5", "6", "7", "8");
+        List.of("1", "2", "2_1", "3", "5", "6", "7", "8", "9");
     private static final List<String> TRANSFER_WORKS_FAQ_ENTRIES =
         List.of("1", "2", "3");
     private static final List<String> TRANSFER_QUESTIONS_FAQ_ENTRIES =
