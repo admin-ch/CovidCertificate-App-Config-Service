a0ca0041bc088352d7a537e6afcef1be70cf13ca
Fabian Aggeler <aggeler@ubique.ch>
2021-11-24 11:35:14 +0100

    Revert "fix tests"
    This reverts commit f9e1e5f0ffa60d49a606b3324d06c279537bb1c4.

--- ch-covidcertificate-backend-config/ch-covidcertificate-backend-config-wallet-ws/src/test/java/ch/admin/bag/covidcertificate/backend/config/wallet/ws/BaseControllerTest.java
+++ ch-covidcertificate-backend-config/ch-covidcertificate-backend-config-wallet-ws/src/test/java/ch/admin/bag/covidcertificate/backend/config/wallet/ws/BaseControllerTest.java
@@ -191,7 +191,7 @@ public abstract class BaseControllerTest {
                         .getResponse();
         ConfigResponse resp =
                 testHelper.toConfigResponse(result, acceptMediaType, TestHelper.PATH_TO_CA_PEM);
-        ConfigAsserter.assertIsNoForceUpdate(resp);
+        ConfigAsserter.assertIsForceUpdate(resp);
     }
 
     @Test
