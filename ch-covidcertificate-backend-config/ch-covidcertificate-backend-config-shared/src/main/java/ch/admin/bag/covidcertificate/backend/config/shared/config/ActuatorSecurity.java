6376318a3f4e9bf35d55193adb54ccdc6373e27c
Martin Alig <alig@ubique.ch>
2021-12-13 13:02:56 +0100

    Remove unused import

--- ch-covidcertificate-backend-config/ch-covidcertificate-backend-config-shared/src/main/java/ch/admin/bag/covidcertificate/backend/config/shared/config/ActuatorSecurity.java
+++ ch-covidcertificate-backend-config/ch-covidcertificate-backend-config-shared/src/main/java/ch/admin/bag/covidcertificate/backend/config/shared/config/ActuatorSecurity.java
@@ -1,7 +1,6 @@
 package ch.admin.bag.covidcertificate.backend.config.shared.config;
 
 import ch.admin.bag.covidcertificate.backend.config.shared.config.configbeans.ActuatorSecurityConfig;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.boot.actuate.info.InfoEndpoint;
